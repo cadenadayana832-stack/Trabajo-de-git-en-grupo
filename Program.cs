b4b43eaf85cad432df282c3e4677a707b507218f
Dayanacadena <dayanacadena03@gmail.com>
2026-02-26 22:14:12 -0500

    Proyecto base inicial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_de_git_en_grupo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

d368577ce0951753fa8490da74d9599466fe5607
cerkiaDev <erickdvid32@gmail.com>
2026-02-27 22:35:42 -0500

    Implementación zapatos

---
+++ Zapatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_de_git_en_grupo
+{
+    internal class Zapatos
+    {
+        public Zapatos() {
+            string[] zapatos = { "Nike", "Adidas", "Puma", "Reebok", "Vans" };
+
+            Console.WriteLine("=== LISTA DE MARCAS DE ZAPATOS ===");
+
+            for (int i = 0; i < zapatos.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {zapatos[i]}");
+            }
+
+            Console.WriteLine("==================================");
+            Console.WriteLine($"Total: {zapatos.Length} marcas");
+        }
+    }
+}

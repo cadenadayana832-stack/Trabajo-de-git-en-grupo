0968c6d67afcf5616436d318d8717c463e0b9a27
Daniel <daniaguirre004@gmail.com>
2026-02-27 23:06:41 -0500

    verduras implementacion.

---
+++ verduras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_de_git_en_grupo
+{
+    internal class verduras
+    {
+        static void Main(string[] args)
+        {
+            string[] verduras = { "Zanahoria", "Brócoli", "Espinaca", "Tomate", "Pepino" };
+
+            Console.WriteLine("=== LISTA DE VERDURAS ===");
+
+            for (int i = 0; i < verduras.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {verduras[i]}");
+            }
+
+            Console.WriteLine("=======================");
+            Console.WriteLine($"Total: {verduras.Length} verduras");
+            Console.WriteLine("Mi verdura favorita es: " + verduras[0]);
+        }
+    }
+}

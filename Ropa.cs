168a789d7e452bd7f41060cbcf51fbb5d39c7108
Daniel <daniaguirre004@gmail.com>
2026-02-27 22:31:42 -0500

    implementacion ropa

---
+++ Ropa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_de_git_en_grupo
+{
+    internal class Ropa
+    {
+        static void Main()
+        {
+            string[] prendas = { "Camisa", "Pantalón", "Vestido", "Chaqueta" };
+
+            Console.WriteLine("=== TIENDA DE ROPA ===");
+
+            for (int i = 0; i < prendas.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {prendas[i]}");
+            }
+
+            Console.WriteLine($"Total: {prendas.Length} prendas");
+        }
+    }
+
+}
